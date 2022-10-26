50d35d038700f5d381bd49787af6ccfdff2a237e
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-10-26 12:48:17 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course3/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course3.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
